831009207b2433fcc2ed44ff35857e26ab5d2903
marek-konieczny <marek.konieczny1@gmail.com>
2021-11-19 16:09:31 +0100

    Initial commit

---
+++ src/main/java/com/example/kodillabytemanipulation/reflection/Student.java
@@ -0,0 +1,12 @@
+package com.example.kodillabytemanipulation.reflection;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Student {
+    private String indexNumber = RandomStringUtils.randomAlphabetic(10);
+
+    public Student() {
+    }
+
+
+}
